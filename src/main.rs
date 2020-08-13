7be2c90366abd421fd6c5c4f33591dd75a7c0467
Dre Mahaarachchi <dre@berkeley.edu>
2020-08-13 13:46:49 -0700

    booting & vga printing functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 // main.rs
-
 #![no_std]
+#![no_main]
 
-fn main() {}
+extern crate rlibc;
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle] //don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    panic!("Some panic message");
+}
+
+// this is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
