0d92184b4e4effabd2661e91b773b36b56abd41b
Dre Mahaarachchi <dre@berkeley.edu>
2020-08-14 09:56:45 -0700

    built out testing framework for unit and integration tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -51,6 +51,7 @@ impl ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
+//>
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
@@ -140,5 +141,29 @@ lazy_static! {
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
+}
+
+// TESTS
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
 
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,42 +1,26 @@
-// main.rs
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rustybean::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(rustybean::test_runner)]
 
 use rustybean::println;
 use core::panic::PanicInfo;
 
-#[no_mangle] //don't mangle the name of this function
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-
-// this is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rustybean::test_panic_handler(info)
 }
 
-/// TESTING
-
-
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
