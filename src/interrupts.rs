61d41b6758ccd1a8901e8176d0b3d3784c9b75d0
Dre Mahaarachchi <dre@berkeley.edu>
2020-08-14 18:01:13 -0700

    finished fault handler base

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 use lazy_static::lazy_static;
+use crate::gdt;
 
 
 //temporary while heap has not been created yet
@@ -8,6 +9,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.divide_error.set_handler_fn(divide_by_zero_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
@@ -17,9 +23,21 @@ pub fn init_idt() {
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: &mut InterruptStackFrame
-) {
+    stack_frame: &mut InterruptStackFrame)
+{
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn divide_by_zero_handler(
+    stack_frame: &mut InterruptStackFrame) 
+{
+    println!("EXCEPTION: attempted to divide by zero\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut InterruptStackFrame, _error_code: u64) -> !
+{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 // TESTS
@@ -28,3 +46,8 @@ extern "x86-interrupt" fn breakpoint_handler(
 fn test_breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
 }
+
+// #[test_case]
+// fn test_divide_by_zero_exception() {
+//     let i = 1 / 0;
+// }
--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,17 @@ pub extern "C" fn _start() -> ! {
 
     rustybean::init();
 
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    };
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
 
     println!("No crash!");
-    
+
     loop {}
 }
 
